f787576aa08bf1f2c58de3268d63363fd7324395
Vitor <lacerdadesouza@outlook.com>
2022-08-29 23:51:00 -0300

    put & delet

---
+++ teste-api-cypress/cypress/integration/produtos.spec.js
@@ -0,0 +1,115 @@
+/// <reference types="cypress" />
+
+describe('Teste da funcionalidade produtos', () => {
+    let token
+    before(() => {
+        cy.token('fulano@qa.com', 'teste').then(tkn => { token = tkn })
+    });
+
+    it('Listar produtos', () => {
+        cy.request({
+            method: 'GET',
+            url: 'produtos'
+
+        }).then(response => {
+            //expect(response.body.produtos[10].nome).to.equal('Capa de celular')
+            expect(response.status).to.equal(200)
+            expect(response.body).to.have.property('produtos')
+            expect(response.duration).to.be.lessThan(30)
+        })
+
+    });
+
+    it('Cadastrar produto', () => {
+        let produto = `Produto EBAC = ${Math.floor(Math.random() * 100000000)}`
+        cy.request({
+            method: 'POST',
+            url: 'produtos',
+            body: {
+                "nome": produto,
+                "preco": 200,
+                "descricao": "new product",
+                "quantidade": 1000
+            },
+            headers: { authorization: token }
+        }).then(response => {
+            expect(response.status).to.equal(201)
+            expect(response.body.message).to.equal('Cadastro realizado com sucesso')
+
+        })
+
+    });
+
+    it('Deve validar mensagem de erro ao cadastrar produto repetido', () => {
+        cy.cadastrarProduto(token, "Produto EBAC = 81080519", 250, "descricao do produto novo", 180)
+
+            .then(response => {
+                expect(response.status).to.equal(400)
+                expect(response.body.message).to.equal('Já existe produto com esse nome')
+
+            })
+    });
+
+    it('Deve editarr um produto ja cadastrado', () => {
+        cy.request('produtos').then(response => {
+            let id = response.body.produtos[0]._id
+            cy.request({
+                method: 'PUT',
+                url: `produtos/${id}`,
+                headers: { authorization: token },
+                body: {
+                    "nome": "Produto EBAC = 42994242",
+                    "preco": 200,
+                    "descricao": "new product",
+                    "quantidade": 1000
+                }
+            })
+        }).then(response => {
+            expect(response.body.message).to.equal('Registro alterado com sucesso')
+            expect(response.status).to.equal(200)
+        })
+
+    });
+
+    it('Deve editar um produto cadastrado previamente', () => {
+        let produto = `Produto EBAC = ${Math.floor(Math.random() * 100000000)}`
+        cy.cadastrarProduto(token, produto, 250, "descricao do produto novo", 180)
+            .then(response => {
+                let id = response.body._id
+
+                cy.request({
+                    method: 'PUT',
+                    url: `produtos/${id}`,
+                    headers: { authorization: token },
+                    body: {
+                        "nome": produto,
+                        "preco": 200,
+                        "descricao": "new product",
+                        "quantidade": 1000
+                    }
+                })
+            }).then(response => {
+                expect(response.body.message).to.equal('Registro alterado com sucesso')
+                expect(response.status).to.equal(200)
+            })
+    });
+
+    it('Deve deletar um produto previamente cadastrado', () => {
+        let produto = `Produto EBAC = ${Math.floor(Math.random() * 100000000)}`
+        cy.cadastrarProduto(token, produto, 250, "descricao do produto novo", 180)
+        .then(response =>{
+            let id = response.body._id
+            cy.request({
+                method: 'DELETE',
+                url: `produtos/${id}`,
+                headers: {authorization: token}
+            }).then(response => {
+                expect(response.body.message).to.equal('Registro excluído com sucesso')
+                expect(response.status).to.equal(200)
+            })
+        })
+
+        
+    });
+
+});
